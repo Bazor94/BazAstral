cf78e919ca578cbf7b985f06b0f7e3c39a7ad40f
Wiktor Urbańczyk <wurbanczyk1@sii.pl>
2025-03-26 22:23:57 +0100

    Init

--- logger.py
+++ logger.py
@@ -13,6 +13,25 @@ class TerminalHandler(logging.Handler):
         log_entry = self.format(record)
         print(log_entry)
 
+
+class FileHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.asteroid_file = open('asteroid.txt', 'w')
+        # TODO reszta plikow
+
+
+    def emit(self, record):
+        record.planet = getattr(record, "planet", "None")
+        record.action = getattr(record, "action", "None")
+        log_entry = self.format(record)
+        
+        if record.action == "asteroid":
+            file = self.asteroid_file
+        # TODO reszta akcji
+
+        file.write(record)
+
 class WidgetHandler(logging.Handler):
     def __init__(self, asteroid_widget, expedition_widget, rest_widget):
         super().__init__()
@@ -40,4 +59,9 @@ logger = logging.getLogger(__name__)
 terminal_handler = TerminalHandler()
 terminal_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(action)s | %(planet)s | %(message)s'))
 logger.addHandler(terminal_handler)
+
+file_handler = FileHandler()
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(planet)s | %(message)s'))
+logger.addHandler(file_handler)
+
 logger.setLevel(logging.INFO)
